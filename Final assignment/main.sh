1bfb08900d16018498bef37b7c5adb75859e296a
Laura Heij <scur2194@int4.local.snellius.surf.nl>
2026-03-13 19:50:55 +0100

    unfrozen dino

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -2,9 +2,9 @@ wandb login
 
 python3 train.py \
     --data-dir ./data/cityscapes \
-    --batch-size 16 \
+    --batch-size 8 \
     --epochs 20 \
-    --lr 0.001 \
+    --lr 0.005 \
     --num-workers 10 \
-    --seed 42 \
-    --experiment-id "U-net-data-augmentation"
+    --seed 11 \
+    --experiment-id "dinov2-unfrozen-lr0005"
